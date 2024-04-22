38ea3f31c5caf9837b3825f53b50b96942fdb6b2
p-baptista <phfb.br@gmail.com>
2024-04-22 15:50:05 -0300

    Added default images to User and Game models.

--- RankD/main_app/urls.py
+++ RankD/main_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
@@ -9,4 +11,4 @@ urlpatterns = [
     path("profile/<str:username>/", views.ProfileView.as_view(), name="profile"),
     path("<str:username>/review-page/<str:gamename>", views.GameReviewView.as_view(), name="review-game"),
     path("<str:username>/add-review/<str:gamename>", views.AddGameReviewView.as_view(), name="add-game-review"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
