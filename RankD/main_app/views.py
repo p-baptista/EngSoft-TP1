2577ed536294bac64bb98c87055e1da9f2689094
p-baptista <phfb.br@gmail.com>
2024-04-22 18:12:41 -0300

    Fixed friends' profiles routes

--- RankD/main_app/views.py
+++ RankD/main_app/views.py
@@ -123,13 +123,18 @@ class ProfileView(ListView):
         context = super().get_context_data(**kwargs)
         
         user = User.objects.filter(username=self.request.resolver_match.kwargs['username']).last()
+        friend = User.objects.filter(username=self.request.resolver_match.kwargs['friend']).last()
 
-        if user:
+        if user and friend:
             context['user'] = user
-            
-            context['user_reviews'] = Review.objects.filter(user_id=user.id)
 
-            context['games_reviewed'] = len(Review.objects.filter(user_id=user.id))
+            context['user_friends'] = [friendship.user2 for friendship in FriendList.objects.filter(user1__username = user.username)]
+
+            context['friend'] = friend
+            
+            context['friend_reviews'] = Review.objects.filter(user_id=friend.id)
+
+            context['games_reviewed'] = len(Review.objects.filter(user_id=friend.id))
             
         return context
 
