8496d748e6b59d732f196ead791d1071294c40ae
p-baptista <phfb.br@gmail.com>
2024-04-22 17:03:59 -0300

    Added reset password template

--- RankD/main_app/urls.py
+++ RankD/main_app/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
     path("signup/", views.SignupView.as_view(), name="signup"),
+    path("reset_password/", views.ResetPasswordView.as_view(), name="reset_password"),
     path("<str:username>/", views.HomeView.as_view(), name="home"),
     path("profile/<str:username>/", views.ProfileView.as_view(), name="profile"),
     path("<str:username>/review-page/<str:gamename>", views.GameReviewView.as_view(), name="review-game"),
